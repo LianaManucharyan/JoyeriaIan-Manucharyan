91b414fd5ae732959a25f1e5c1bbfc4d3e4643f9
Liana Manucharyan <liana.manucharyan002@gmail.com>
2024-11-22 18:34:49 -0300

    Variables de entorno

--- src/db/db.js
+++ src/db/db.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkHJMqsuFoj1u1bOZRYvkuS7P331pNb18",
-  authDomain: "ecommerce-joyeriaian.firebaseapp.com",
-  projectId: "ecommerce-joyeriaian",
-  storageBucket: "ecommerce-joyeriaian.firebasestorage.app",
-  messagingSenderId: "126150611254",
-  appId: "1:126150611254:web:cfd2de72824f86bc27e397"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.VITE_AUTH_DOMAIN,
+  projectId: import.meta.VITE_PROJECT_ID,
+  storageBucket: import.meta.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.VITE_APP_ID
 };
 
 initializeApp(firebaseConfig);
